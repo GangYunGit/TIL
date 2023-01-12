dc8c023111bd21198c96168c58d7c3627276023d
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2023-01-12 09:07:23 +0900

    TIL/React merge

--- React/practicefiles/react-es/public/main.js
+++ React/practicefiles/react-es/public/main.js
@@ -1,36 +1,49 @@
 console.log('안녕 React + Babel Standalone 😃');
 
-const headlineElement = React.createElement(
-  'h1',
-  { className: 'headline' },
-  'h1 태그 입니다.',
-)
+// React component(재사용 가능)을 이용한 React Element 생성
+const Headline = (props) => 
+  React.createElement(
+    'h1',
+    { className: `headline ${props.className}` },
+    props.children
+  );
 
-const abbrebiationElement = React.createElement(
-  'abbr',
-  { key: 'abbr-io', title: 'User Interface' },
-  '속의 abbreviation '
-)
+const Abbreviation = (props) =>
+  React.createElement(
+    'abbr',
+    { key: 'abbr-io', title: 'User Interface' },
+    '속의 abbreviation '
+  );
 
-const catchphriseElement = React.createElement(
-  'p',
-  {
-    className: 'catchprhise',
-    children: [
-      'p태그 ',
-      abbrebiationElement,
-      '입니다.'
-    ]
-  },
-)
+const Catchphrise = () => 
+  React.createElement(
+    'p',
+    {
+      className: 'catchprhise'
+    },
+    'p태그 ',
+    React.createElement(Abbreviation),
+    '입니다.'
+  );
 
-const strictMode = React.createElement(
-  React.StrictMode,
-  null,
-  headlineElement,
-  catchphriseElement
-)
+const App = (props) => 
+  React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(Headline, { className: 'point' },
+      'h1 ',
+      '태그입니다',
+      ' 😊'
+    ),
+    React.createElement(Catchphrise)
+  );
 
 const container = document.getElementById('root');
 const reactDomRoot = ReactDOM.createRoot(container);
-reactDomRoot.render(strictMode);
+reactDomRoot.render(
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(App)
+  )
+);
