c2c2511bbaf2ab4d38e9be2264b347818b885fe0
이강윤 <lky61010@naver.com>
2023-03-08 00:58:07 +0900

    Update TIL/JPA jpashop

---
+++ JPA/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        // JPA에서는 transaction이라는 단위가 매우 중요
+        EntityTransaction tx = em.getTransaction();
+
+        // 엔티티 매니저는 데이터 변경 시 트랜잭션을 시작해주어야 함.
+        tx.begin();
+
+        try {
+
+            Order order = new Order();
+            order.addOrderItem();
+
+            tx.commit();
+        } catch (Exception e) {
+            // EntityManager close
+            em.close();
+        }
+        // EntityManagerFactory close
+        emf.close();
+    }
+}
